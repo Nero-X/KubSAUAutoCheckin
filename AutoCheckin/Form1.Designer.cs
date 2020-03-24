a2841081fc850a12dfc4615fd94d1f9c0bff9e57
Nero-X <SashaJCV@yandex.ru>
2020-03-24 19:50:48 +0300

    Init commit

---
+++ AutoCheckin/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AutoCheckin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Cookie = new System.Windows.Forms.TextBox();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Посещение пар в фоновом режиме!";
+            this.notifyIcon1.BalloonTipTitle = "AutoCheckin";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "AutoCheckin";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".AspNet.ApplicationCookie";
+            // 
+            // textBox_Cookie
+            // 
+            this.textBox_Cookie.Location = new System.Drawing.Point(12, 51);
+            this.textBox_Cookie.Multiline = true;
+            this.textBox_Cookie.Name = "textBox_Cookie";
+            this.textBox_Cookie.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Cookie.Size = new System.Drawing.Size(216, 47);
+            this.textBox_Cookie.TabIndex = 1;
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(161, 21);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(67, 23);
+            this.button_Clear.TabIndex = 2;
+            this.button_Clear.Text = "Очистить";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(83, 120);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 3;
+            this.button_Start.Text = "Старт";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1800000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 163);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.textBox_Cookie);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoCheckin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Cookie;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AutoCheckin/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutoCheckin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WebClient client = new WebClient();
+        Schedule schedule;
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBox_Cookie.Clear();
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            if(!timer1.Enabled)
+            {
+                if (textBox_Cookie.Text == "")
+                {
+                    MessageBox.Show("Введите cookie");
+                    return;
+                }
+
+                client.Headers.Add(HttpRequestHeader.Cookie, ".AspNet.ApplicationCookie=" + textBox_Cookie.Text);
+                int week = Convert.ToInt32(client.UploadString("https://stud.kubsau.ru/Home/GetCurrentWeekNumber", "").Substring(8, 1));
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
+                string resp = client.UploadString("https://stud.kubsau.ru/Home/GetSchedule", "week=" + week);
+                schedule = JsonConvert.DeserializeObject<Schedule>(Decode(resp));
+
+                tryCheck();
+                timer1.Start();
+                button_Start.Text = "Стоп";
+            }
+            else
+            {
+                timer1.Stop();
+                button_Start.Text = "Старт";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /////
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                //notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                //notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tryCheck();
+        }
+
+        void tryCheck()
+        {
+            try
+            {
+                Pair current = schedule.model[0].GetCurrentPair();
+                if (current == null) return;
+                string resp = client.DownloadString($"https://stud.kubsau.ru/Home/Checkin?disciplineName={current.DisciplineName}&classNumber=" + current.LessonNumber);
+                string name = Decode(FindSubstring(resp, "<p class=\"navbar-nav ml-auto\">", "</p>"));
+                if (name != "-1") notifyIcon1.ShowBalloonTip(1000, this.Text, $"{name} посетил пару №{ current.LessonNumber}: \"{current.DisciplineName}\" в {DateTime.Now}", ToolTipIcon.None);
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse resp = (HttpWebResponse)ex.Response;
+                MessageBox.Show(Convert.ToString((int)resp.StatusCode), resp.StatusDescription);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+            }
+        }
+
+        string Decode(string str)
+        {
+            Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+            byte[] utf8Bytes = win1251.GetBytes(str);
+            return Encoding.UTF8.GetString(utf8Bytes);
+        }
+
+        string FindSubstring(string text, string start, string end)
+        {
+            int startIndex = text.IndexOf(start);
+            int endIndex = text.IndexOf(end, startIndex);
+            if (startIndex >= 0 && endIndex >= 0) return text.Substring(startIndex + start.Length, endIndex - startIndex - start.Length);
+            else return "-1";
+        }
+    }
+
+    public class Pair
+    {
+        public string DisciplineName { get; set; }
+        public int LessonNumber { get; set; }
+        private DateTime start;
+        private DateTime end;
+        public DateTime CheckinDateStart { get => start; set => start = value.ToLocalTime(); }
+        public DateTime CheckinDateEnd { get => end; set => end = value.ToLocalTime(); }
+    }
+
+    public class Model
+    {
+        public List<Pair> Pairs { get; set; }
+
+        public Pair GetCurrentPair()
+        {
+            foreach (Pair pair in Pairs)
+            {
+                if (pair.CheckinDateStart < DateTime.Now && pair.CheckinDateEnd > DateTime.Now) return pair;
+            }
+            return null;
+        }
+    }
+
+    public class Schedule
+    {
+        public List<Model> model { get; set; }
+    }
+}
